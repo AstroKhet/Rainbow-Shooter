c71f2519ee06216c1a478bef645db39f57c46dc1
SaxyCat <67085313+SaxyCat@users.noreply.github.com>
2021-05-18 23:21:28 +0800

    renamed some variables

--- projectile.py
+++ projectile.py
@@ -7,27 +7,27 @@ class Projectile:
         self.x = x
         self.y = y
         self.radius = radius
-        self.sx = pos_stop[0]
-        self.sy = pos_stop[1]
+        self.stop_x = pos_stop[0]
+        self.stop_y = pos_stop[1]
         self.travel_pos = []  # 16 positions, 4 frames/pos, 64 frames/s
         self.frames = frames
-        # Large bubble
+
         self.particles = []
         self.particle_count = random.randint(4, 10)
 
     def initialize(self):
-        x_dist = self.sx - self.x
-        y_dist = self.sy - self.y
+        x_dist = self.stop_x - self.x
+        y_dist = self.stop_y - self.y
         x_intervals = [self.x + (x_dist / 128) * i for i in range(128)]
         y_intervals = [self.y + (y_dist / 128) * i for i in range(128)]
 
         self.travel_pos = [(x, y) for x, y in zip(x_intervals, y_intervals)]
 
         for i in range(self.particle_count):  # Particles
-            x_pos = random.randint(self.sx - 160, self.sx + 160)
-            y_pos = random.randint(self.sy - 160, self.sy + 160)
-            x_dist = x_pos - self.sx
-            y_dist = y_pos - self.sy
+            x_pos = random.randint(self.stop_x - 160, self.stop_x + 160)
+            y_pos = random.randint(self.stop_y - 160, self.stop_y + 160)
+            x_dist = x_pos - self.stop_x
+            y_dist = y_pos - self.stop_y
             x_intervals = [self.travel_pos[-1][0] + (x_dist / 64) * i for i in range(64)]
             y_intervals = [self.travel_pos[-1][1] + (y_dist / 64) * i for i in range(64)]
             travel_pos = [(x, y) for x, y in zip(x_intervals, y_intervals)]
